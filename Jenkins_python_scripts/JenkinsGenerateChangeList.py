9e505a744fb37ac0837d82545f71d2e4f0f94fa7
YuxingShi <shiyuxing1988@gmail.com>
2022-09-14 10:44:27 +0800

    完成按照单号生成变更列表功能； 完成linux系统下功能调试；

--- Jenkins_python_scripts/JenkinsGenerateChangeList.py
+++ Jenkins_python_scripts/JenkinsGenerateChangeList.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 import re
-
+import os
 from colorama import init
 from colorama import Fore
 import subprocess
@@ -26,30 +26,51 @@ if is_py3:
 
 
 class SVN(object):
-    params = ["svn"]
-    revision_dict = {}
-    project_dict = {}
     project_name_list = None
+    child_working_path_dict = {}
 
-    def __init__(self, svn_url: str, svn_usr: str, svn_passwd: str, svn_message: str, project_names: str):
+    def __init__(self, svn_url: str, svn_usr: str, svn_passwd: str, project_names: str,
+                 working_path: str):
         """
         根据SVN提交日志生成变更列表
-        :param svn_url:
-        :param svn_usr:
-        :param svn_passwd:
+        :param svn_url: svn根路径
+        :param svn_usr: svn用户
+        :param svn_passwd: svn密码
         :param svn_message: svn提交时候填写的信息
-        :param project_names:
+        :param project_names: 要生成变更列表的工程名
+        :param working_path: 脚本工作路径
         """
         self.url = svn_url
         self.usr = svn_usr
         self.passwd = svn_passwd
-        self.message_head = svn_message
         self.project_name_list = project_names.split(',')
+        self.working_path = working_path
+        self.init_child_working_path()
 
     @staticmethod
     def write2file(filename: str, content: str):
         with open(filename, 'w') as fp:
             fp.write(content)
+
+    def init_child_working_path(self):
+        """
+        child_working_path初始化
+        :return:
+        """
+        if not os.path.isdir(self.working_path):
+            print(Fore.RED + '【{}】该工作路径非目录！'.format(self.working_path))
+            exit(1)
+        self.child_working_path_dict.clear()
+        for dir_name in os.listdir(self.working_path):
+            dev_match = re.findall('DR(\d{8,})(\d{3,})', dir_name)  # 匹配PP系统开发单号
+            if len(dev_match) > 0:
+                child_dir = os.path.join(self.working_path, dir_name)
+                if os.path.isfile(child_dir):
+                    continue
+                else:
+                    dev_date, dev_sn = dev_match[0]
+                    self.child_working_path_dict[dir_name] = {'path': child_dir, 'date': dev_date}
+        # print('self.child_working_path_dict', self.child_working_path_dict)
 
     def execute_svn_command(self, params: list):
         svn_output = ''
@@ -75,41 +96,44 @@ class SVN(object):
                     svn_output = cp.stdout.decode('utf-8')  # .split('\r\n')[3:-4]  # [-1].decode('utf-8')[:-1]
             else:
                 print(Fore.RED + "执行SVN出错!【%s】" % (params))
-        print(svn_output)
         return svn_output
 
-    def get_revision_list_after_date(self, date='2022-09-06'):
+    def get_revision_list_after_date(self, date: str):
         """
         将指定日期后的SVN日志按天生成列表
-        :param date: 日期字符串
+        :param date: 日期字符串，如：20220909
         :return: 日志列表
         """
-        self.params.extend(('log', self.url, '-r', '{%s}:HEAD' % date, '-v'))
+        params = ['svn']
+        params.extend(('log', self.url, '-r', '{%s}:HEAD' % date, '-v'))
         # pattern_log_split = '-{72,}\r\n(.*?)\r\n-{72,}'
-        #pattern_revision = '\r\n(.*?)\r\nChanged paths:\r\n(.*?)\r\n\r\n(.*?)\r\n'
+        # pattern_revision = '\r\n(.*?)\r\nChanged paths:\r\n(.*?)\r\n\r\n(.*?)\r\n'
         pattern_revision = '\n(.*?)\n改变的路径: \n(.*?)\n\n(.*?)\n'
-        svn_log = self.execute_svn_command(self.params)
+        svn_log = self.execute_svn_command(params)
         revision_list = re.findall(pattern_revision, svn_log, flags=re.DOTALL)
-        print('revision_list', revision_list)
+        # print('revision_list', revision_list)
         return revision_list
 
-    def init_revision_dict_by_message_contain(self):
-        for info, changelist, message in self.get_revision_list_after_date():
-            if not message.startswith(self.message_head):
-                print('提交信息头不包含单号【{}】！略过！'.format(self.message_head))
+    @staticmethod
+    def get_revision_dict_by_message_contain(message_head: str, revision_list: list):
+        revision_dict = {}
+        for info, changelist, message in revision_list:
+            if not message.startswith(message_head):
+                # print(Fore.YELLOW + '提交信息头不包含单号【{}】！略过！'.format(message_head))
                 continue
             info_list = info.split(' | ')
-            changelist = changelist.split('\r\n')
-            self.revision_dict[info_list[0]] = {'info_list': info_list, 'changelist': changelist}
-        print('self.revision_dict', self.revision_dict)
+            changelist = changelist.split('\n')  # linux下为\n
+            revision_dict[info_list[0]] = {'info_list': info_list, 'changelist': changelist}
+        # print('revision_dict', revision_dict)
+        return revision_dict
 
-    def merge_changelist(self):
+    def merge_changelist(self, revision_dict: dict):
         """
-        按合并变更列表
+        合并变更列表
         :return:
         """
-        self.init_revision_dict_by_message_contain()
-        for key, value in self.revision_dict.items():
+        project_dict = {}
+        for key, value in revision_dict.items():
             changelist = value.get('changelist')
             for path in changelist:
                 flag, svn_path = re.findall('   ([MDA]) /(.*)', path, flags=re.DOTALL)[0]
@@ -117,21 +141,21 @@ class SVN(object):
                     try:
                         project_name_index = svn_path.index(project_name)
                         project_path = svn_path[project_name_index - 1:]
-                        if self.project_dict.get(project_name) is None:
-                            self.project_dict[project_name] = [(flag, project_path)]
+                        if project_dict.get(project_name) is None:
+                            project_dict[project_name] = [(flag, project_path)]
                         else:
-                            self.project_dict.get(project_name).append((flag, project_path))
+                            project_dict.get(project_name).append((flag, project_path))
                     except ValueError:
                         continue
-        print('self.project_dict', self.project_dict)
+        #print('project_dict', project_dict)
+        return project_dict
 
-    def generate_changelist_file(self):
+    def generate_changelist_file(self, project_dict: dict, working_path: str):
         """
         生成变更列表txt
         :return:
         """
-        self.merge_changelist()
-        for project_name, flag_path in self.project_dict.items():
+        for project_name, flag_path in project_dict.items():
             temp_dict = {}
             for flag, path in flag_path:
                 if temp_dict.get(flag) is None:
@@ -147,7 +171,16 @@ class SVN(object):
                 elif temp_key == 'D':
                     text += '#删除文件\n#' + '#\n#'.join(set(temp_value)) + '\n\n'
             filename = '{}路径说明.txt'.format(project_name)
-            self.write2file(filename, text)
+            file_path = os.path.join(working_path, filename)
+            self.write2file(file_path, text)
+            print(Fore.GREEN + '生成变更列表【{}】'.format(file_path))
+
+    def batch_generate_changelist_file(self):
+        for key, value in self.child_working_path_dict.items():
+            revision_list = self.get_revision_list_after_date(value.get('date'))
+            revision_dict = self.get_revision_dict_by_message_contain(key, revision_list)
+            project_dict = self.merge_changelist(revision_dict)
+            self.generate_changelist_file(project_dict, value.get('path'))
 
 
 def usage():
@@ -166,8 +199,9 @@ def usage():
 
 if __name__ == '__main__':
     import jenkinsconf
+
     if (len(sys.argv) != 4):
         usage()
-    svn = SVN(svn_url=sys.argv[1], svn_usr=jenkinsconf.svnusr, svn_passwd=jenkinsconf.svnpwd, svn_message=sys.argv[2],
-              project_names=sys.argv[3])
-    svn.generate_changelist_file()
+    svn = SVN(svn_url=sys.argv[1], svn_usr=jenkinsconf.svnusr, svn_passwd=jenkinsconf.svnpwd, project_names=sys.argv[2],
+              working_path=sys.argv[3])
+    svn.batch_generate_changelist_file()
